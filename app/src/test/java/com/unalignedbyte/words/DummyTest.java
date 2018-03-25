b3067482e811806543d8781798e317a90bbe26ad
Rafal Grodzinski <rafal@unalignedbyte.com>
2018-03-26 00:10:02 +0100

    Reenabled tests and added a dummy test

---
+++ app/src/test/java/com/unalignedbyte/words/DummyTest.java
@@ -0,0 +1,12 @@
+package com.unalignedbyte.words;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DummyTest {
+    @Test
+    public void testDummy() throws Exception {
+        assertTrue(false);
+    }
+}
